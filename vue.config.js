45abcbdd3469c8370823414fabbd6258dc7a6e82
jian玄冰 <jianxuanhuo1@126.com>
2020-02-16 23:11:52 +0800

    init: 初始化项目

---
+++ vue.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  publicPath: "/",
+  assetsDir: "webjars",
+  outputDir: "dist",
+  lintOnSave: false,
+  productionSourceMap: false,
+  indexPath: "doc.html",
+  pwa: {
+    iconPaths: {
+      favicon32: "favicon.ico",
+      favicon16: "favicon.ico",
+      appleTouchIcon: "favicon.ico",
+      maskIcon: "favicon.ico",
+      msTileImage: "favicon.ico"
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+  devServer: {
+    proxy: {
+      "/": {
+        target: 'http://localhost:25899/',
+        ws: true,
+        changeOrigin: true
+      },
+      "/swagger":{
+        target:'http://localhost:25899/',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+};
